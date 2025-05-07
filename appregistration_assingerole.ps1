bf9132d345fc84cd55a00d2e4f4c8a5faf2d57f5
✨ ollie ✨ <124591524+oliverfarthing@users.noreply.github.com>
2025-05-07 10:57:05 +0100

    Add files via upload

---
+++ appregistration_assingerole.ps1
@@ -0,0 +1,28 @@
+﻿# Install the module.
+# Install-Module Microsoft.Graph -Scope CurrentUser
+
+# Your tenant ID (in the Azure portal, under Azure Active Directory > Overview).
+$tenantID = ''
+
+# The name of your web app, which has a managed identity that should be assigned to the server app's app role.
+$webAppName = ''
+$resourceGroupName = ''
+
+# The name of the server app that exposes the app role.
+$serverApplicationName = '' # For example, MyApi
+
+# The name of the app role that the managed identity should be assigned to.
+$appRoleName = 'Proposal.Read' # For example, MyApi.Read.All
+
+# Look up the web app's managed identity's object ID.
+$managedIdentityObjectId = (Get-AzWebApp -ResourceGroupName $resourceGroupName -Name $webAppName).identity.principalid
+
+Connect-MgGraph -TenantId $tenantId -Scopes 'Application.Read.All','Application.ReadWrite.All','AppRoleAssignment.ReadWrite.All','Directory.AccessAsUser.All','Directory.Read.All','Directory.ReadWrite.All'
+
+# Look up the details about the server app's service principal and app role.
+$serverServicePrincipal = (Get-MgServicePrincipal -Filter "DisplayName eq '$serverApplicationName'")
+$serverServicePrincipalObjectId = $serverServicePrincipal.Id
+$appRoleId = ($serverServicePrincipal.AppRoles | Where-Object {$_.Value -eq $appRoleName }).Id
+
+# Assign the managed identity access to the app role.
+New-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $serverServicePrincipalObjectId -PrincipalId $managedIdentityObjectId -ResourceId $serverServicePrincipalObjectId -AppRoleId $appRoleId
---
+++ exchange_autoexpandingarchiveenabled.ps1
@@ -0,0 +1,4 @@
+﻿Get-Mailbox -ResultSize Unlimited | Where-Object { -not $_.AutoExpandingArchiveEnabled } | ForEach-Object {
+    Enable-Mailbox -Identity $_.Identity -AutoExpandingArchive
+    Write-Host "Enabled auto-expanding archive for $($_.UserPrincipalName)"
+}
