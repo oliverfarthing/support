06c93738417b7d3afb3dde536a12c31293c8a379
✨ ollie ✨ <124591524+oliverfarthing@users.noreply.github.com>
2025-06-20 14:20:08 +0100

    Add files via upload

---
+++ certificate_build_server.ps1
@@ -0,0 +1,14 @@
+﻿$dnsNames = @(
+    ""
+)
+
+$cert = New-SelfSignedCertificate `
+    -DnsName $dnsNames `
+    -CertStoreLocation "Cert:\LocalMachine\My" `
+    -KeyExportPolicy Exportable `
+    -KeyLength 2048 `
+    -KeySpec KeyExchange `
+    -Provider "Microsoft RSA SChannel Cryptographic Provider" `
+    -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.1") `
+    -HashAlgorithm "SHA256" `
+    -NotAfter (Get-Date).AddYears(10)
