56d1304fdb77a13e356d22347834e1669bc604d0
✨ ollie ✨ <124591524+oliverfarthing@users.noreply.github.com>
2025-06-20 16:25:51 +0100

    Delete exchange_configure_autoexpandingarchive.ps1

--- exchange_configure_autoexpandingarchive.ps1
+++
@@ -1,4 +0,0 @@
-﻿Get-Mailbox -ResultSize Unlimited | Where-Object { -not $_.AutoExpandingArchiveEnabled } | ForEach-Object {
-    Enable-Mailbox -Identity $_.Identity -AutoExpandingArchive
-    Write-Host "Enabled auto-expanding archive for $($_.UserPrincipalName)"
-}
